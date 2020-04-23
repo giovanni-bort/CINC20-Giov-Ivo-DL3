a90afeca42c560a07436869e4d1b97e0bf4e1e2a
giovanni-bort <63361927+giovanni-bort@users.noreply.github.com>
2020-04-23 18:03:01 +0200

    Add files via upload

--- load_12ECG_model.m
+++ load_12ECG_model.m
@@ -1,6 +1,7 @@
 function model = load_12ECG_model()
      % CINC20_ivaylo_P2
-    global out_labels_1 out_labels_2
+    global out_labels_1 out_labels_2 KK_ERROR
+    KK_ERROR=0;
     out_labels_1(1:9)=0;
     out_labels_2(1:9)=0;
          filename='trainedGN_04_06.mat';
